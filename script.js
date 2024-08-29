a30b118f666c7b63a3263bdaee7c052cff49dfb1
Deepak Sharma <144551642+Deepak9997@users.noreply.github.com>
2024-08-29 12:52:35 +0530

    added

---
+++ script.js
@@ -0,0 +1,11 @@
+function menu(){
+    let navp3 = document.getElementById('navp3');
+    let mnu = document.getElementById('mnu');
+    mnu.classList.toggle('show');
+}
+
+
+function remove(){
+    let mnu = document.getElementById('mnu');
+    mnu.classList.toggle('hide')
+}
